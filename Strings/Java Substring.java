7165b87dfa3e6bab429769c89772caf23fd90241
Aditya <Adityapurviya67@gmail.com>
2026-02-23 09:08:23 +0000

    A meaningful commit message

---
+++ Strings/Java Substring.java
@@ -0,0 +1,22 @@
+/* *
+* Author: Aditya Purviya
+* URL: https://github.com/Adityapurviya4
+* */
+
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String s = sc.nextLine();
+         int Start = sc.nextInt();
+         int end = sc.nextInt();
+          
+      System.out.println(s.substring(Start,end));
+    }
+}
