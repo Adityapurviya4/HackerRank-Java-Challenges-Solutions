c0805b853e14459c23d97d01cf5596895ef130ad
Aditya <Adityapurviya67@gmail.com>
2026-03-01 10:18:50 +0000

    big decimal

---
+++ BigNumber/Java BigDecimal.java
@@ -0,0 +1,33 @@
+/* *
+* Author: Aditya Purviya
+* URL: https://github.com/Adityapurviya4
+* */
+
+import java.util.*;
+class Solution{
+
+    public static void main(String []args){
+        //Input
+        Scanner sc= new Scanner(System.in);
+        int n=sc.nextInt();
+        String []s=new String[n+2];
+        for(int i=0;i<n;i++){
+            s[i]=sc.next();
+        }
+      	sc.close();
+
+        //Write your code here
+Arrays.sort(s, 0, n, new Comparator<String>() {
+ @Override public int compare(String a, String b) { 
+    BigDecimal A1 = new BigDecimal(a);
+     BigDecimal A2 = new BigDecimal(b); 
+     return A2.compareTo(A1);
+      } });
+        //Output
+        for(int i=0;i<n;i++)
+        {
+            System.out.println(s[i]);
+        }
+    }
+
+}
